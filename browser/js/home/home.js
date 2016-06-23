768b84389e12652164abb68f0f9b5f54b9bf3c9b
coreygreen1108 <coreygreen1108@gmail.com>
2016-06-23 16:04:28 -0400

    query factory

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,25 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
+        controller: 'HomeCtrl',
         templateUrl: 'js/home/home.html'
     });
 });
+
+app.controller('HomeCtrl', function($scope, QueryFactory){
+	// QueryFactory.getCategories()
+	// .then(function(categories){
+	
+	// })
+
+	// // $scope.categorySelect = function(category) {
+	// // 	QueryFactory.
+	// // }
+
+	// QueryFactory.getDatasets({category: 'recreation', count: 500})
+	// .then(function(data) {
+	// 	console.log(QueryFactory.getColumns(data[78]));
+
+	// })
+})
+
---
+++ browser/js/query/query.factory.js
@@ -0,0 +1,29 @@
+app.factory('QueryFactory', function ($http) {
+
+	var obj = {
+		getCategories: function(){
+			return $http.get('https://api.us.socrata.com/api/catalog/v1/categories')
+			.then(function(categories){
+				return categories.data.results;
+			})
+		},
+		getDatasets: function(category){
+			return $http.get('https://api.us.socrata.com/api/catalog/v1?categories=' + category.category + '&only=datasets&limit=3000')
+			.then(function(set){
+				return set.data.results
+			})
+				
+		},
+		getColumns: function(dataset){
+			return dataset.resource.columns_name;
+		},
+		getData: function(dataset, arrOfColumns){
+			return $http.get(dataset.permalink + '.json?$select=' + arrofColumns.join(","))
+			.then(function(set){
+				return set.data.results
+			})
+		}
+	}
+
+	return obj
+})
