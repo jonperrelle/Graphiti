e7f4ff2f15b0c8197245c319c272cb54949f6a25
Geoff Bass <geoff.c.bass@gmail.com>
2016-06-23 16:24:09 -0400

    syntactic cleanup

--- browser/js/query/query.factory.js
+++ browser/js/query/query.factory.js
@@ -1,17 +1,17 @@
 app.factory('QueryFactory', function ($http) {
 
-	var obj = {
+	let obj = {
 		getCategories: function(){
 			return $http.get('https://api.us.socrata.com/api/catalog/v1/categories')
 			.then(function(categories){
 				return categories.data.results;
-			})
+			});
 		},
 		getDatasets: function(category){
 			return $http.get('https://api.us.socrata.com/api/catalog/v1?categories=' + category.category + '&only=datasets&limit=3000')
 			.then(function(set){
-				return set.data.results
-			})
+				return set.data.results;
+			});
 				
 		},
 		getColumns: function(dataset){
@@ -20,10 +20,10 @@ app.factory('QueryFactory', function ($http) {
 		getData: function(dataset, arrOfColumns){
 			return $http.get(dataset.permalink + '.json?$select=' + arrofColumns.join(","))
 			.then(function(set){
-				return set.data.results
-			})
+				return set.data.results;
+			});
 		}
 	}
 
-	return obj
-})
+	return obj;
+});
