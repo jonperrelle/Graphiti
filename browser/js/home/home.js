d1044dbcb0a664281d7eb5a68c35c9f9f0948219
Geoff Bass <geoff.c.bass@gmail.com>
2016-06-23 16:21:53 -0400

    remove home controller

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -5,21 +5,3 @@ app.config(function ($stateProvider) {
         templateUrl: 'js/home/home.html'
     });
 });
-
-app.controller('HomeCtrl', function($scope, QueryFactory){
-	// QueryFactory.getCategories()
-	// .then(function(categories){
-	
-	// })
-
-	// // $scope.categorySelect = function(category) {
-	// // 	QueryFactory.
-	// // }
-
-	// QueryFactory.getDatasets({category: 'recreation', count: 500})
-	// .then(function(data) {
-	// 	console.log(QueryFactory.getColumns(data[78]));
-
-	// })
-})
-
