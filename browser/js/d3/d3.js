68bf86c77c4ee2fcdaf9aee9d09096159493309a
coreygreen1108 <coreygreen1108@gmail.com>
2016-06-28 12:16:58 -0400

    Update d3.js

--- browser/js/d3/d3.js
+++ browser/js/d3/d3.js
@@ -16,7 +16,7 @@
       var scriptTag = $document[0].createElement('script');
       scriptTag.type = 'text/javascript'; 
       scriptTag.async = true;
-      scriptTag.src = '/d3/d3.min.js';
+      scriptTag.src = 'http://d3js.org/d3.v3.min.js';
       scriptTag.onreadystatechange = function () {
         if (this.readyState == 'complete') onScriptLoad();
       };
