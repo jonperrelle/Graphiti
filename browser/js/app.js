a66c66d3bbf6f0368aabd1b740c8c367f87b6d6f
Geoff Bass <geoff.c.bass@gmail.com>
2016-07-01 11:14:57 -0400

    remove rootscope event listener

--- browser/js/app.js
+++ browser/js/app.js
@@ -50,9 +50,4 @@ app.run(function ($rootScope, AuthService, $state) {
             }
         });
     });
-
-    // $rootScope.$on('$stateChangeSuccess',function(){
-    //     console.log("state change")
-    //     document.querySelector("html, body").animate({ scrollTop: 0 }, 200);
-    // })
 });
