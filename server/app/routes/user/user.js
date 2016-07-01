ca7309030090e62e2822e18a17219dab48032587
Zachary Levine <zrlevine@gmail.com>
2016-07-01 18:40:45 -0400

    adding security

--- server/app/routes/user/user.js
+++ server/app/routes/user/user.js
@@ -3,18 +3,37 @@ const router = require('express').Router();
 const db = require('../../../db');
 const User = db.model('user');
 const Promise = require('bluebird');
+const HttpError = require('../../../utils/HttpError');
 
+router.param('userId', function (req, res, next, id) {
+  User.findById(id)
+  .then(function (user) {
+    if (!user) throw HttpError(404);
+    req.requestedUser = user;
+    next();
+  })
+  .catch(next);
+});
 
-router.get('/:userId', function (req, res, next) {
-    User.findById(req.params.userId)
-    .then(function (user) {
-        return Promise.all([user.getDatasets(),user.getGraphs()]) 
+function assertIsLoggedIn (req, res, next) {
+  if (req.user) next();
+  else next(HttpError(401));
+}
+
+// function assertAdmin (req, res, next) {
+//   if (req.user && req.user.isAdmin) next();
+//   else next(HttpError(403));
+// }
+
+router.get('/:userId',assertIsLoggedIn, function (req, res, next) {
+    
+    	let user =  req.requestedUser;
+
+        Promise.all([user.getDatasets(),user.getGraphs()]) 
         .spread(function(datasets,graphs) {
             res.send({user, datasets, graphs});
         })
         .catch(next);
-    })
-    .catch(next);
 });
 
 
