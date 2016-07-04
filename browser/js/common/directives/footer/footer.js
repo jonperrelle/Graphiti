bd7cf5da3a3689f905c0f9c600e11619d7d0f0c1
coreygreen1108 <coreygreen1108@gmail.com>
2016-07-04 14:22:22 -0400

    footer is sticky

---
+++ browser/js/common/directives/footer/footer.js
@@ -0,0 +1,13 @@
+app.directive('footer', function ($rootScope, AuthService, AUTH_EVENTS, $state) {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/footer/footer.html',
+
+        // link: function (scope, element) { 
+
+        // }
+
+    };
+
+});
