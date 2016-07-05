be977fad88de36a2e482f9f6a15c8299e3fa3305
Jon Perrelle <jonperrelle@gmail.com>
2016-07-05 13:12:41 -0400

    fixed bug with graph persistence

--- browser/js/common/directives/login/login.directive.js
+++ browser/js/common/directives/login/login.directive.js
@@ -4,13 +4,12 @@ app.directive('loginForm', function ($state, AuthService,$stateParams,$localStor
         templateUrl: 'js/common/directives/login/login.directive.html',
         scope: { },
         link: function(scope) {
+            scope.source = $stateParams.source || null;
         	scope.sendLogin = function (loginInfo) {
-
                 scope.error = null;
-
                 AuthService.login(loginInfo).then(function (user) {
-                    if($stateParams.source === 'dataset') $state.go('datasetDetails',{datasetId: $localStorage.datasetId});
-                    else if ($stateParams.source === 'graph') $state.go('singleGraph');
+                    if(scope.source === 'dataset') $state.go('datasetDetails',{datasetId: $localStorage.datasetId});
+                    else if (scope.source === 'graph') $state.go('singleGraph');
                     else $state.go('userHome', {userId: user.id});
                 }).catch(function () {
                     scope.error = 'Invalid login credentials.';
