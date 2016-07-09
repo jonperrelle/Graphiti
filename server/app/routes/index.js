841fd6bf4fadb4c6087c6334d406207926bdbae6
Zachary Levine <zrlevine@gmail.com>
2016-07-09 14:39:09 -0400

    styled oauth buttons

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,7 +2,6 @@
 var router = require('express').Router();
 module.exports = router;
 
-router.use('/members', require('./members'));
 router.use('/soda',require('./soda/soda.router.js'));
 router.use('/upload',require('./upload/upload.js'));
 router.use('/users',require('./user/user.js'));
