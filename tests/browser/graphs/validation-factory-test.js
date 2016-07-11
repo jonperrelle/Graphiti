b226d94e222d6bb6a334caf713a9984f5649de99
Geoff Bass <geoff.c.bass@gmail.com>
2016-07-11 00:40:00 -0400

    write tests for number validation

--- tests/browser/graphs/validation-factory-test.js
+++ tests/browser/graphs/validation-factory-test.js
@@ -1,7 +1,8 @@
 describe('Validation Factory', function () {
   beforeEach(module('Graphiti'));
 
-  let ValidationFactory;
+  let ValidationFactory,
+      testNum;
 
   beforeEach('Get factory', inject(function (_ValidationFactory_) {
     ValidationFactory = _ValidationFactory_;
@@ -9,5 +10,41 @@ describe('Validation Factory', function () {
 
   it('should be an object', function () {
     expect(ValidationFactory).to.be.an('object');
+  });
+
+  describe('validateNumber', function () {
+    
+
+    it('should be a function', function () {
+      expect(ValidationFactory.validateNumber).to.be.a('function');
+    });
+
+    let nonNumbers = [{nonNum: 'one'}, 
+                {nonNum: 'two'}, 
+                {nonNum: 'three'}, 
+                {nonNum: 'four'}],
+        nonNumCol = {name: 'nonNum'},
+        numbers = [{num: '1'}, 
+                {num: '2'}, 
+                {num: '3'}, 
+                {num: '4'}],
+        numCol = {name: 'num'},
+        almostNumbers = [{thing: 'a1'}, 
+                {thing: '2'}, 
+                {thing: '3 in a million'}, 
+                {thing: 'four'}],
+        almostNumCol = {name: 'thing'};
+
+    it('should return false for columns containing non-numbers', function () {
+      expect(ValidationFactory.validateNumber(nonNumbers, nonNumCol)).to.equal(false);
+    });
+
+    it('should return true for columns containing numbers', function () {
+      expect(ValidationFactory.validateNumber(numbers, numCol)).to.equal(true);
+    });
+
+    it('should return false for columns with any non-number data', function () {
+      expect(ValidationFactory.validateNumber(almostNumbers, almostNumCol)).to.equal(false);
+    });
   });
 });
